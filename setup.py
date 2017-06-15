f203efef810650ead7341d0afc66510f6842d9db
Rasmus Ansin <rasmus.ansin+github@gmail.com>
2017-06-15 18:42:59 +0200

    Add Github link to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='assertionhelper',
       version='0.1',
       description='Helpers for doing assertions with logging',
-      url='http://github.com/????',
+      url='http://github.com/Ran4/assertionhelper',
       author='Rasmus Ansin',
       author_email='rasmus.ansin@gmail.com',
       license='MIT',
